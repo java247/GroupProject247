97d145bda837d065dcf5eccb9d33b00430f615c8
java247 <34145081+java247@users.noreply.github.com>
2017-12-04 22:04:35 -0500

    Add files via upload

---
+++ src/com/company/error.java
@@ -0,0 +1,9 @@
+interface error {
+    public static String errorText(){
+        return "ERROR! Please enter correct input.";
+    }
+
+    public static String noPizzaText(){
+        return "You ordered no pizzas, Please have a nice day";
+    }
+}
